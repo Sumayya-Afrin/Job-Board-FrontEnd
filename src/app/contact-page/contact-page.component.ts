63a75453d9f1c33852dfc3c7e4bc865d565dac7a
Sumayya-Afrin <sumayyafrin.mohammad@proclink.com>
2024-12-11 12:41:35 +0530

    worked on contact page & home page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
